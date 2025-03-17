a5c7cd88e50004f9f5ed7f57ab45e9768d8ed790
TE109 <1991tmce@gmail.com>
2025-03-17 18:38:52 -0400

    Initial Commit

---
+++ Mobile_Managerio_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile_Managerio_Project
+//
+//  Created by user271259 on 3/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mobile_Managerio_ProjectTests/Mobile_Managerio_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_Managerio_ProjectTests.swift
+//  Mobile_Managerio_ProjectTests
+//
+//  Created by user271259 on 3/17/25.
+//
+
+import Testing
+@testable import Mobile_Managerio_Project
+
+struct Mobile_Managerio_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
